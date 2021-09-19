6232da00959c32f1cbaf4428a17764a8ba5d03ef
Rushiraj11 <grushiraj95@gmail.com>
2021-09-19 19:47:39 +0530

    Added: Welcome message

---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.moodanalyser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    //@Test
+//    public void givenMessage_WhenProper_ShouldReturnSad() {
+//        MoodAnalyser moodanalyser=new MoodAnalyser();
+//        String actualResult=moodanalyser.analyseMood("User is Sad");
+//        Assert.assertEquals("Sad",actualResult);
+    }
+
+//}
